b795c7e90f611f8f3cc12be0e1d175d711a9061d
jmilind1234 <jmilind1234@gmail.com>
2025-08-02 11:07:12 +0530

    Made payload for /signup api dynamic.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const {connectToCluster} = require("./config/database");
+
 const UserModel = require("./Models/userModel");
 const app = express();
 
-app.post("/signup", async (req, res, next) => {
-    const userObj = {
-        firstName : "Milind",
-        lastName: "Jain",
-        emailId: "jmilind1234@gmail.com",
-        password: "Mjilind@1999",
-        age: 25,
-        gender: "male"
-    }
-    
+app.post("/signup", express.json(),async (req, res, next) => {
     try {
-        const response = await UserModel.create({...userObj});
+        const response = await UserModel.create({...req.body});
         res.send(response);
     } catch (error) {
         console.log("Error while saving user in the collection ", error);
