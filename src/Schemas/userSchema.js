d96b1c96fc302c7f05a80a8e109ffaeb1f731d92
jmilind1234 <jmilind1234@gmail.com>
2025-08-03 14:16:27 +0530

    added constraints on the fields of the user schema.

--- src/Schemas/userSchema.js
+++ src/Schemas/userSchema.js
@@ -2,12 +2,25 @@ const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
-    firstName: String,
+    firstName: {
+        type:String,
+        required: true
+    },
     lastName: String,
-    emailId: String,
-    password: String,
+    emailId: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type:String,
+        required: true
+    },
     age: Number,
     gender: String,
+    photoUrl: String,
+    about: {type:String, default: "I'm a developer!!!"},
+    skills: [String]
 });
 
 module.exports = {userSchema};
