fcafdb125e7acdb29f9cc173cc05e2f6da26ec78
jmilind1234 <jmilind1234@gmail.com>
2025-08-09 22:45:23 +0530

    deleted unrequired APIs and added more detailed error response codes for /signup api.

--- src/app.js
+++ src/app.js
@@ -44,6 +44,9 @@ app.get("/profile", async(req, res, next)=>{
     try {
         const decoded = jwt.verify(req.cookies.token, "DevTinder");
         const userData = await UserModel.findById(decoded?.userId);
+        if(!userData){
+            res.status(404).send("User not found!!!");
+        }
         res.send(userData);
     } catch (error) {
         res.send(500).send("Can't get profile of the user. Please login again!!!");
@@ -59,72 +62,17 @@ app.post("/signup", async (req, res, next) => {
         const response = await UserModel.create({...req.body, password: encryptedPassword});
         res.send(response);
     } catch (error) {
+        console.log("Error name is ",error.name);
         if(error instanceof BadRequestError){
             res.status(400).send({Message: error.message});
         }
+        if(error.name === "ValidationError"){
+            res.status(400).send({Message: error.message});
+        }
+        if(error.name === "MongoServerError"){
+            res.status(409).send({Message : error.message});
+        }
         res.status(500).send("Error saving the user " +  error.message);
-    }
-});
-
-//get user by email
-app.get("/user", async(req, res, next)=>{
-    try {
-        const emailId = req.body.emailId;
-        const user = await UserModel.find({emailId});
-        console.log("user with required email id is ", user);
-
-        res.send(user[0]);
-    } catch (error) {
-        console.log("error ", error);
-        res.status(500).send("Problem while pulling out user by email  - ", error.message);
-    }
-})
-
-//Get all users
-app.get("/feed", async(req, res, next)=>{
-    try {
-        const users = await UserModel.find({});
-        console.log("users in collections are ----", users);
-        res.send(users);
-    } catch (error) {
-        res.status(500).send("Problem while fetching the users details ",error);
-    }
-})
-
-//API to delete user from collection
-app.delete("/user", async(req, res, next)=>{
-    try {
-        const userId = req.body.userId;
-        const response = await UserModel.findByIdAndDelete(userId);
-        res.send(`User with ${userId} deleted successfully`);
-    } catch (error) {
-        res.status(500).send(`Problem in deleteing the user - ${error.message}`);
-    }
-});
-
-//updating the user data based on email address provided
-app.patch("/user/email", async(req, res, next)=>{
-    try {
-        //need to add validation here as well
-        validateUpdateViaEmail(req.body);
-        const {emailId, ...data} = req.body;
-        const response = await UserModel.findOneAndUpdate({emailId},{...data},{runValidators: true});
-        res.send(response);
-
-    } catch (error) {
-        res.status(500).send("Problem in updating userId with provided emailId");
-    }
-})
-
-
-//API to update the user in collection
-app.patch("/user", async(req, res, next)=>{
-    try {
-        const {userId, ...data} = req.body;
-        const response = await UserModel.findByIdAndUpdate(userId, data, {runValidators: true, returnDocument: 'after'});
-        res.send("User was updated successfully" + response);
-    } catch (error) {
-        res.status(500).send(`Problem while updating the user - ${error.message}`);
     }
 });
 
